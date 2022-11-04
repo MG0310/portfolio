0eeef9535864705a1d3e5fbf1fde1ca1ffaae546
moises garcia <moisesgarcia.official@gmail.com>
2022-11-04 00:41:36 -0500

    Add SpringBoot application to Git

---
+++ src/test/java/com/mg0310/portfolio/PortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mg0310.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
